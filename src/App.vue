9bd613ad32b84286afdbe171f64a9abd9cd8a3c0
FawazALhafiz <fawaztwix111@gmail.com>
2025-11-17 20:59:22 +0200

    fix: style issues without general styling in App main component

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,20 @@
 <script setup>
 import Quiz from './components/Quiz.vue'
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background: linear-gradient(to bottom, #f5f5f5, #e8e8e8);
+}
+</style>
